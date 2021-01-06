bf7e32df924778c4b09b30270bf62186aacafeb3
阿栋的仓库 <2675413003@qq.com>
2021-01-06 12:56:50 +0800

    ‘’

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header/>
+    <div class="main">
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+export default {
+  name: 'App',
+  components: {PostList, Header}
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #e1e1e1;
+}
+.main {
+  width: 74%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Article.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="article">
+    <div>
+      <div class="topic_header">
+        <div class="topic_title">
+          <ul>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  data() {
+    return {
+      post: {}
+    }
+  },
+  methods: {
+    getArticleData() {
+      this.$http.get(`https://cnodejs.org/api/v1/topic/${this.$route.params.id}`)
+      .then(res =>{
+        console.log('---')
+          console.log(res)
+          this.post = res.data.success
+
+      })
+      .catch(err => {
+        console.log(err)
+        console.log(this.$route.params.id)
+      })
+    }
+  },
+  beforeMount() {
+    this.getArticleData()
+  }
+}
+
+</script>
+
+<style scoped>
+@import url('../assets/markdown-github.css');
+.topbar {
+  padding: 10px;
+  background-color: #f6f6f6;
+  height: 16px;
+  font-size: 12px;
+  margin-top: 10px;
+}
+
+.article:not(:first-child) {
+  margin-right: 340px;
+  margin-top: 15px;
+}
+
+#reply, .topic_header {
+  background-color: #fff;
+}
+
+#reply {
+  margin-top: 15px;
+}
+
+#reply img {
+  width: 30px;
+  height: 30px;
+  position: relative;
+  bottom: -9px;
+}
+
+#reply a, #reply span {
+  font-size: 13px;
+  color: #666;
+  text-decoration: none;
+}
+.replySec{
+  border-bottom:1px solid #e5e5e5;
+  padding:0 10px;
+}
+
+.loading {
+  text-align: center;
+  padding-top: 300px;
+}
+
+.replyUp a:nth-of-type(2) {
+  margin-left: 0px;
+  display: inline-block;
+}
+
+.topic_header {
+  padding: 10px;
+}
+
+.topic_title {
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 8px;
+}
+
+.topic_header ul {
+  list-style: none;
+  padding: 0px 0px;
+  margin: 6px 0px;
+}
+
+.topic_header li {
+  display: inline-block;
+  font-size: 12px;
+  color: #838383;
+}
+
+.topic_content {
+  border-top: 1px solid #e5e5e5;
+  padding: 0 10px;
+}
+
+.markdown-text img {
+  width: 92% !important;
+}
+</style>
